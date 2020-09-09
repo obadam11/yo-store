b59817278a22c4665d18a1ed7fd29f5c5e3cbc67
obadam11 <obada.mik.ab@gmail.com>
2020-09-09 19:21:02 +0300

    setup (Firebase and typescript)

---
+++ dist/index.js
@@ -0,0 +1,23 @@
+"use strict";
+var firebaseConfig = {
+    apiKey: "AIzaSyAz8HtrKT-UiKutaaOXs1Kwfg2IdNTrovk",
+    authDomain: "yo-project-68c34.firebaseapp.com",
+    databaseURL: "https://yo-project-68c34.firebaseio.com",
+    projectId: "yo-project-68c34",
+    storageBucket: "yo-project-68c34.appspot.com",
+    messagingSenderId: "748825923070",
+    appId: "1:748825923070:web:294e68a40f6a6f4c61c4af",
+    measurementId: "G-TZWJZYZ157"
+};
+firebase.initializeApp(firebaseConfig);
+var db = firebase.firestore();
+console.log("hey there");
+db.collection("Test").doc("new").set({
+    "worked": true
+})
+    .then(function () {
+    console.log("worked");
+})
+    .catch(function (err) {
+    console.log(err);
+});
